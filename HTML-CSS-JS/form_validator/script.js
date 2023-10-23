90d9adde81efd070d1614af4ff86c81ee53487f5
yoshipon-tech <yoshipon.tech@gmail.com>
2023-10-23 21:32:34 +0900

    本日分

--- HTML-CSS-JS/form_validator/script.js
+++ HTML-CSS-JS/form_validator/script.js
@@ -1,9 +1,10 @@
 const form = document.getElementById('form');
 const username = document.getElementById('username');
 const email = document.getElementById('email');
-const password = document.getElementById('fpassword');
+const password = document.getElementById('password');
 const password2= document.getElementById('password2');
 
+// Show input error message
 function showError(input, message) {
   const formControl = input.parentElement;
   formControl.className = 'form-control error';
@@ -11,12 +12,17 @@ function showError(input, message) {
   small.innerText = message;
 }
 
+// Show success outline
 function showSuccess(input) {
   const formControl = input.parentElement;
-  formControl.className = 'form-control error';
+  formControl.className = 'form-control success';
 }
 
-
+// Checkemail is valid
+function isValidEmail(email) {
+  const re = /^[a-zA-Z0-9_+-]+(.[a-zA-Z0-9_+-]+)*@([a-zA-Z0-9][a-zA-Z0-9-]*[a-zA-Z0-9]*\.)+[a-zA-Z]{2,}$/;
+  return re.test(String(email).toLowerCase())
+}
 
 // Event listener
 form.addEventListener('submit', function(e) {
@@ -27,5 +33,27 @@ form.addEventListener('submit', function(e) {
   } else {
     showSuccess(username);
   }
+
+  if(email.value === '') {
+    showError(email, 'Email is required')
+  } else if(!isValidEmail(email.value)) {
+    showError(email, 'Email is not valid')
+  } else {
+    showSuccess(email);
+  }
+
+  if(password.value === '') {
+    showError(password, 'Password is required')
+  } else {
+    showSuccess(password);
+  }
+
+  if(password2.value === '') {
+    showError(password2, 'Password2 is required')
+  } else {
+    showSuccess(password2);
+  }
+
+
   console.log(username.value);
 });
